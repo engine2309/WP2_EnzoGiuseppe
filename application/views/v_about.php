098a0f7137ad045e18a232f33f15b2dd25a5aa31
enzogine2309 <enzogiussepe037@gmail.com>
2024-03-28 12:31:42 +0700

    Membuat Template sederhana

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section> 
+<h1><?php echo $judul ?></h1>
+<h4>Nama</h4> 
+    <ul type="disc"> 
+        <li>Nama Depan : Enzo</li> 
+        <li>Nama Belakang : Zinedine</li> 
+    </ul> 
+    <br> 
+    <h4>Alamat</h4> 
+    <ul type="none"> 
+        <li> Perumahan Kartika Wanasari Blok C2 No 20</li> 
+    </ul> 
+     
+    <h4>Tempat Lahir</h4> 
+    <ul type="none"> 
+        <li>Bekasi</li> 
+    </ul> 
+ 
+    <h4>Olah Raga Favorit</h4> 
+    <ul type="square"> 
+        <li>Futsal</li> 
+        <li>Badminton</li> 
+    </ul> 
+</section>
